435e98d6e160f36839597f4b794d6d5859c43b86
r0neel <ronbhagwakar@gmail.com>
2022-03-15 09:13:49 +0000

    Update game controller to match game model methods

--- controller/gameController.js
+++ controller/gameController.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const router = express.router();
+const router = express.Router();
 const Game = require("../Models/Game");
 
+//games index
 router.get("/", async (req, res) => {
   try {
     const games = await Game.all;
@@ -11,38 +12,61 @@ router.get("/", async (req, res) => {
   }
 });
 
+//get leaderboard
 router.get("/leaderboard", async (req, res) => {
   try {
-    const leaderboard = await Game.showLeaderboard;
+    const leaderboard = await Game.leaderboard;
     res.status(200).json(leaderboard);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get("/:id/", async (req, res) => {
-  try {
-    const game = Game.findById(req.params.id);
-    res.status(200).json(game);
-  } catch (err) {
-    res.status(404).json(err);
-  }
-});
-
+//create lobby
 router.post("/", async (req, res) => {
   try {
-    Game.start(req.body.options);
-    res.status(201).json("Game created");
+    const lobbyName = req.body.name;
+    const hostUsername = req.body.host;
+    const options = req.body.options;
+
+    const game = await Game.createGame(lobbyName, hostUsername, options);
+    await game.startGame();
+    //start game via socketio
+    res.status(201).json("Game started");
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
+//join lobby
+router.post("/:id", async (req, res) => {
+  try {
+    const game = await Game.findById(req.params.id);
+    await game.joinGame(req.body.username);
+    //connect user to game via socketio
+  } catch (err) {
+    res.status(404).json(err);
+  }
+});
+
+//restart game
 router.post("/:id/restart", async (req, res) => {
   try {
-    const game = Game.findById(req.params.id);
-    game.start(req.body.options);
+    const game = await Game.findById(req.params.id).updateOptions(
+      req.body.options
+    );
+    await game.startGame();
+    //start game via socketio
     res.status(201).send("Game restarted");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    await Game.findById(req.params.id).delete();
+    res.status(200).json("Game ended");
   } catch (err) {
     res.status(500).json(err);
   }
