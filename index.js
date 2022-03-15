c24518b7e57e85a73f84de48e6fcce436bcce3b6
r0neel <ronbhagwakar@gmail.com>
2022-03-15 09:16:06 +0000

    initialise socket connection

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const app = require("./app");
-const port = process.env.PORT || 3000;
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000"
+  }
+});
 
-app.listen(port, () => {
-    console.log(`Express just departed from port ${port}!`);
+const port = process.env.PORT || 8000;
+
+httpServer.listen(port, () => {
+  console.log(`Express just departed from port ${port}!`);
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected"); // runs when client first connects
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+
+  socket.on("message", (message) => {
+    console.log(message);
+  });
 });
