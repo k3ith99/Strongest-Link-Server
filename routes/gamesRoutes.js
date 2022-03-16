864467be9ce5e63587db7c88d5c92e55c6077808
r0neel <32517170+r0neel@users.noreply.github.com>
2022-03-16 17:15:56 +0000

    Update gamesRoutes.js

--- routes/gamesRoutes.js
+++ routes/gamesRoutes.js
@@ -6,9 +6,7 @@ const gameController = require("../controller/gameController");
 router.get("/leaderboard", gameController.leaderboard);
 
 router.get("/", gameController.index);
-<<<<<<< HEAD
 router.get("/:id/restart", gameController.restart);
-=======
 router.get("/:name", gameController.show);
 >>>>>>> 0dc54e0e63d8706356254c3ecc308819737f3377
 router.post("/", gameController.create);
