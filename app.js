2853f63ae0f93f8c1014699c73235f010ae92761
r0neel <ronbhagwakar@gmail.com>
2022-03-16 08:51:05 +0000

    add socket io to listen on server start

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-app.use(cors());
-app.use(express.json());
-
+const { createServer } = require("http");
+const server = createServer(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://localhost:3000"
+  }
+});
+global.io = io;
 const gameRoutes = require("./controller/gameController");
 
+app.use(cors());
+app.use(express.json());
 app.use("/games", gameRoutes);
-
 app.get("/", (req, res) => {
   res.send("server is up and running :)");
 });
 
-module.exports = app;
+io.on("connection", (socket) => {
+  console.log(socket.id);
+  socket.on("startgame", () => {
+    console.log(socket.id);
+  });
+  socket.on("createroom", (lobbyName) => {
+    socket.join(`${lobbyName}`);
+  });
+  socket.on("joinroom", (lobbyName) => {
+    socket.join(`${lobbyName}`);
+  });
+});
+
+module.exports = server;
