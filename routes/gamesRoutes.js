1701eb0ad0789ab025efeba413b7c80ac1a74e05
r0neel <32517170+r0neel@users.noreply.github.com>
2022-03-16 17:16:09 +0000

    Update gamesRoutes.js

--- routes/gamesRoutes.js
+++ routes/gamesRoutes.js
@@ -8,7 +8,6 @@ router.get("/leaderboard", gameController.leaderboard);
 router.get("/", gameController.index);
 router.get("/:id/restart", gameController.restart);
 router.get("/:name", gameController.show);
->>>>>>> 0dc54e0e63d8706356254c3ecc308819737f3377
 router.post("/", gameController.create);
 router.post("/:name", gameController.join);
 router.delete("/:id", gameController.deleteGame);
