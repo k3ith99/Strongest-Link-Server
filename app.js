a37aa88360be1c4a6e5e495a3902f61e087a9607
r0neel <ronbhagwakar@gmail.com>
2022-03-16 10:45:35 +0000

    Fix: createroom socket emit

--- app.js
+++ app.js
@@ -5,13 +5,16 @@ const { createServer } = require("http");
 const server = createServer(app);
 const io = require("socket.io")(server, {
   cors: {
-    origin: "http://localhost:3000"
+    origin: [
+      "http://localhost:3000",
+      "https://amritb.github.io/socketio-client-tool/"
+    ]
   }
 });
 global.io = io;
 
 const gameRoutes = require("./routes/gamesRoutes");
-
+const gameController = require("./controller/gameController");
 app.use(cors());
 app.use(express.json());
 app.use("/games", gameRoutes);
@@ -20,15 +23,23 @@ app.get("/", (req, res) => {
 });
 
 io.on("connection", (socket) => {
-  console.log(socket.id);
-  socket.on("startgame", () => {
-    console.log(socket.id);
-  });
-  socket.on("createroom", (lobbyName) => {
-    socket.join(`${lobbyName}`);
+  socket.on("startgame", async ({ lobbyName, roomId }) => {
+    console.log(lobbyName, roomId);
+    const game = await gameController.startGame(roomId);
+    io.to(lobbyName).emit("game", game);
   });
   socket.on("joinroom", (lobbyName) => {
     socket.join(`${lobbyName}`);
+    io.to(lobbyName).emit("game", "sup");
+  });
+  socket.on("answer", (user, answer) => {
+    const response = gameController.makeTurn(
+      socket.adapter.rooms[0],
+      user,
+      answer
+    );
+    console.log(response);
+    io.emit("response", { response });
   });
 });
 
