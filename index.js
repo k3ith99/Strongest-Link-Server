909428273da8be469a3d1e5896162fe0b99b3a62
r0neel <ronbhagwakar@gmail.com>
2022-03-16 08:51:54 +0000

    set http-server to listen

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
-const app = require("./app");
-const { createServer } = require("http");
-const { Server } = require("socket.io");
-const httpServer = createServer(app);
-const io = new Server(httpServer, {
-  cors: {
-    origin: "http://localhost:3000"
-  }
-});
-const socketController = require("./controller/socketController");
-
+const server = require("./app");
 const port = process.env.PORT || 8000;
 
-httpServer.listen(port, () => {
+server.listen(port, () => {
   console.log(`Express just departed from port ${port}!`);
 });
-
-io.on("connection", socketController.respond); // runs when client first connects
